25c9b7091246609835428000060d2b1eef35cfbd
duganlx <840797783@qq.com>
2023-11-17 11:00:59 +0800

    添加将ipynb文件转换成md文件脚本

--- py/mains/eampysdk_use.py
+++ py/mains/eampysdk_use.py
@@ -24,21 +24,35 @@ def gsfhelpConf():
     eamsdk = EamPySdk()
     data = [
         ['id', 'title', 'filepath', 'createTime', 'updateTime', 'publish', 'archive'],
-        [1, '进化论代码管理规范及使用指引', '/examples/code-control/CodeControl.md', '2023-05-01', '2023-05-01', 1, '1. 信息安全专题'],
-        [2, '进化论文件类数据管理规范', '/examples/dataopts/data_file_opts.md', '2023-06-01', '2023-06-01', 1, '1. 信息安全专题'],
-        [3, 'Remote-SSH的配置方式', '/examples/ssh_vscode/ConfigRemoteSSH.md', '2022-11-01', '2022-11-01', 1, '2. 环境配置专题'],
-        [4, 'Dev Container的配置方式', '/examples/ssh_vscode/DevContainerGsf2.md', '2022-12-01', '2022-12-01', 1, '2. 环境配置专题'],
-        [5, '在服务器上安装配置Docker环境', '/examples/config-docker/configure_docker_env.md', '2022-12-01', '2022-12-01', 1, '2. 环境配置专题'],
-        [6, '将gsf服务托管至非内网环境', '/examples/hosting-dataservice/hosting_dataservice.md', '2023-08-01', '2023-08-01', 1, '2. 环境配置专题'],
-        [7, 'gsfctl工具的使用', '/examples/gsfctl/GsfCtlHelp.md', '2023-05-01', '2023-05-01', 1, '3. 金葵花2.0开发专题'],
-        [8, 'gsf2从服务器构建环境和快速使用', '/examples/gsfctl/GsfQuickGuide.md', '2023-01-01', '2023-01-01', 1, '3. 金葵花2.0开发专题'],
-        [9, '如何开发用户的工具库，且在多项目中共享使用', '/examples/gsfctl/develop_shared_conan_package.md', '2023-01-01', '2023-01-01', 1, '3. 金葵花2.0开发专题'],
-        [10, 'pygsf使用 - PyGsfRpc', '/examples/pygsf/pygsf_rpc.md', '2023-11-08', '2023-11-08', 1, '3. 金葵花2.0开发专题'],
-        [11, 'pygsf使用 - Oms', '/examples/pygsf/pygsf_oms.md', '2023-11-08', '2023-11-08', 1, '3. 金葵花2.0开发专题'],
-        [12, 'pygsf使用 - Model', '/examples/pygsf/pygsf_model.md', '2023-11-08', '2023-11-08', 1, '3. 金葵花2.0开发专题'],
-        [13, 'pygsf使用 - Backtest', '/examples/pygsf/pygsf_backtest.md', '2023-11-08', '2023-11-08', 1, '3. 金葵花2.0开发专题'],
-        [14, '高效哈希表phmap', '/examples/phmap/phmap.md', '2023-08-01', '2023-08-01', 1, '4. 开发笔记专题'],
-        [15, 'C++ memory order', '/examples/memory-model/memory-order.md', '2023-09-01', '2023-09-01', 1, '4. 开发笔记专题']
+        # [1, '进化论代码管理规范及使用指引', '/examples/code-control/CodeControl.md', '2023-05-01', '2023-05-01', 1, '1. 信息安全专题'],
+        # [2, '进化论文件类数据管理规范', '/examples/dataopts/data_file_opts.md', '2023-06-01', '2023-06-01', 1, '1. 信息安全专题'],
+        # [3, 'Remote-SSH的配置方式', '/examples/ssh_vscode/ConfigRemoteSSH.md', '2022-11-01', '2022-11-01', 1, '2. 环境配置专题'],
+        # [4, 'Dev Container的配置方式', '/examples/ssh_vscode/DevContainerGsf2.md', '2022-12-01', '2022-12-01', 1, '2. 环境配置专题'],
+        # [5, '在服务器上安装配置Docker环境', '/examples/config-docker/configure_docker_env.md', '2022-12-01', '2022-12-01', 1, '2. 环境配置专题'],
+        # [6, '将gsf服务托管至非内网环境', '/examples/hosting-dataservice/hosting_dataservice.md', '2023-08-01', '2023-08-01', 1, '2. 环境配置专题'],
+
+        # 1. gsfctl 系列
+        [7, 'gsfctl 从服务器构建环境和快速使用', '/examples/gsfctl/GsfQuickGuide.md', '2023-01-01', '2023-01-01', 1, '1. gsfctl 系列'],
+        [8, 'gsfctl 工具的使用', '/examples/gsfctl/GsfCtlHelp.md', '2023-05-01', '2023-05-01', 1, '1. gsfctl 系列'],
+        [9, 'gsfctl 如何开发用户的工具库，且在多项目中共享使用', '/examples/gsfctl/develop_shared_conan_package.md', '2023-01-01', '2023-01-01', 1, '1. gsfctl 系列'],
+
+        # 2. pygsf系列
+        [10, 'pygsf使用 - PyGsfRpc', '/examples/pygsf/pygsf_rpc.md', '2023-11-08', '2023-11-08', 1, '2. pygsf系列'],
+        [11, 'pygsf使用 - Oms', '/examples/pygsf/pygsf_oms.md', '2023-11-08', '2023-11-08', 1, '2. pygsf系列'],
+        [12, 'pygsf使用 - Model', '/examples/pygsf/pygsf_model.md', '2023-11-08', '2023-11-08', 1, '2. pygsf系列'],
+        [13, 'pygsf使用 - Backtest', '/examples/pygsf/pygsf_backtest.md', '2023-11-08', '2023-11-08', 1, '2. pygsf系列'],
+
+        # 3. gsfctl 系列
+        [20, '高频数据服务（DataService) 框架：WWDS', '/examples/WWDS/WWDS.md', '2023/10/20', '2023/10/20', 1, '3. gsfctl 系列'],
+        [21, '高频Touch类算法（Algo）框架：WWAlgo', '/examples/WWAlgo/WWAlgo.md', '2023/10/21', '2023/10/21', 1, '3. gsfctl 系列'],
+
+        # 4. DataService系列
+        [22, 'BarDs 使用', '/examples/bards/bards.md', '2023/11/16', '2023/11/16', 1, '4. DataService系列'],
+        [23, 'KLineDs 使用', '/examples/klineds/klineds.md', '2023/11/16', '2023/11/16', 1, '4. DataService系列'],
+
+        # 6. 开发笔记专题
+        # [14, '高效哈希表phmap', '/examples/phmap/phmap.md', '2023-08-01', '2023-08-01', 1, '6. 开发笔记专题'],
+        # [15, 'C++ memory order', '/examples/memory-model/memory-order.md', '2023-09-01', '2023-09-01', 1, '6. 开发笔记专题']
     ]
 
     eamsdk.uploadData(
@@ -144,11 +158,11 @@ def biclassify(generate=False):
 
 
 if __name__ == '__main__':
-    # gsfhelpConf()
+    gsfhelpConf()
     # adsStatementStatus()
     # stockTAanalysis(local=False)
 
-    biclassify(generate=False)
+    # biclassify(generate=False)
     # rnn = torch.nn.LSTM(input_size=10, hidden_size=20, num_layers=2, bidirectional=True)
     # input = torch.randn(5, 3, 10)#(seq_len, batch, input_size)
     # h0 = torch.randn(4, 3, 20) #(num_layers,batch,output_size)
